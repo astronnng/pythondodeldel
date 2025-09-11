6b17f10a651cb9a5160714e4d41dc3f3e163aac7
Astroooonngg <93302851+astronnng@users.noreply.github.com>
2025-09-11 08:55:01 -0300

    atualização do codigo

--- saida.py
+++ saida.py
@@ -1,3 +1,8 @@
 print(" Ola cadu seja bem vindo!")
 
-print("O valor inteiro é:", 10)
+print(" O valor inteiro é:", 10)
+
+print(f" O valor em decimal é: {10: d}")
+print(f" O valor em binario é: {10: b}")
+
+print(f" O valor de PI é:{3.14159265: f}")
