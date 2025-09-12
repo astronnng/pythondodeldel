87b1fcb420a59e7a9811d74790aa222fc15b3293
Alexandre <107.xande@gmail.com>
2025-09-11 23:44:22 -0300

    Adicionado esquema de brasileiro

--- idade.py
+++ idade.py
@@ -9,3 +9,15 @@ altura = input("Digite sua altura:")
 print(f"A altura de:", nome, "é:", altura)
 print(f"A altura de", nome, "é:", altura, "com a idade:", idade, "anos")
 #fim do segundo teste#  
+
+
+
+#inicio do segundo teste#
+idade = 0 
+altura = 0
+nome = ""
+
+idade=int(input(f"Digite sua idade:")) 
+altura=(input(f"Digite sua altura:"))
+nome=str(input(f"Digite seu nome:"))
+print(f"{nome} tem {idade} anos e sua altura é {altura}m")
