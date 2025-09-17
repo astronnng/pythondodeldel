a7f9539b6737c960af17448372d13d51df44dffd
Alexandre <107.xande@gmail.com>
2025-09-16 21:51:11 -0300

    Adicionado a7 e calc

---
+++ a7.py
@@ -0,0 +1,20 @@
+
+
+#print(Num1)
+
+#print("\nO valor fixo da variavel é: ", Num1)
+#print("valor da variavel: ", Num1+1)
+#print("valor da variavel: ", Num1-1)
+
+n1=int(input("Digite um numero: "))
+n2=int(input("Digite outro numero: "))
+
+Soma = (n1 + n2)
+Subtracao = (n1 - n2)
+Multi = (n1 * n2)
+
+print(f"O valor da soma dele é: ", Soma)
+print(f"O valor da multiplicação de ele: ", Multi)
+print(f"O valor da subtração dele é: ", Subtracao)
+
+
---
+++ calc.py
@@ -0,0 +1,17 @@
+num1 = float(input("Digite o primeiro numero: "))
+num2 = float(input("Digite o segundo numero: "))
+
+soma = num1 + num2
+subtracao = num1 - num2
+multiplicacao = num1 * num2
+divisao = num1 / num2
+
+print("Resultados: ")
+print("Soma: ", soma)
+print("Subtração: : ", subtracao)
+print("Multiplicação: ", multiplicacao)
+print("Divisão: ", divisao)
+
+
+
+
