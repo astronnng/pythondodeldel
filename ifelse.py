4796aaa65623acf84ea7458fb189ec30b6831545
Astroooonngg <93302851+astronnng@users.noreply.github.com>
2025-09-24 23:41:38 -0300

    Usado o if e else no ex

---
+++ ifelse.py
@@ -0,0 +1,21 @@
+
+'''nota = float(input("Digite a sua nota: "))
+
+if nota >= 7.0:
+    print("Aprovado!!")
+else: ##caso for menor que 7 vai ser reprovado
+    print("Reprovado!!")
+
+'''
+
+
+nome = str(input("Digite seu nome:"))
+idade = float(input("Digite a sua idade: "))
+
+
+if idade >= 18.0:
+    print(nome, "você é maior de idade!!")
+else: 
+    print(nome, "voce é menor de idade!!!")
+
+
