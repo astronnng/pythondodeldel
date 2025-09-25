279813734380eae6b253acfc48a2e2f8fdb130fc
Astroooonngg <93302851+astronnng@users.noreply.github.com>
2025-09-24 23:38:04 -0300

    Ex com if

---
+++ desvioIF.py
@@ -0,0 +1,17 @@
+''''
+nota = float(input("Digite a sua nota: "))
+
+if nota >= 7.0:
+    print("Aprovado!") 
+
+'''
+
+
+nota = float(input("Digite a sua nota: "))
+
+if nota >= 7.0:
+    print("Aprovado!") 
+if nota < 7.0 and nota >= 5.0:
+    print("Tem direito a exame!")
+if nota <= 4.0:
+    print("Reprovado malandro!!")
