e88ec18fe01265d509bc1ba78fa259d33505e9ec
Astroooonngg <93302851+astronnng@users.noreply.github.com>
2025-09-24 23:41:12 -0300

    usado elif e if

---
+++ if_elif.py
@@ -0,0 +1,49 @@
+'''
+dia = int(input("Digite um numero entre 1 e 7:"))
+
+if dia == 1:
+    print("Domingo.")
+elif dia == 2:
+    print("Segunda-feira")
+elif dia == 3:
+    print("Terça-feira")
+elif dia == 4:
+    print("Quarta-feira")
+elif dia == 5:
+    print("Quinta-feira")
+elif dia == 6:
+    print("Sexta-feira")
+elif dia == 7:
+    print("Sabado")
+else: print("Número invalido.")
+
+
+'''
+
+dia = int(input("Digite um numero entre 1 e 12:"))
+
+if dia == 1:
+    print("Janeiro.")
+elif dia == 2:
+    print("Feveiro")
+elif dia == 3:
+    print("Março")
+elif dia == 4:
+    print("Abril")
+elif dia == 5:
+    print("Maio")
+elif dia == 6:
+    print("Junho")
+elif dia == 7:
+    print("Julho")
+elif dia == 8:
+    print("Agosto")
+elif dia == 9:
+    print("Setembro")
+elif dia == 10:
+    print("Outubro")
+elif dia == 11:
+    print("Novembro")
+elif dia == 12:
+    print("Dezembro")    
+else: print("Número invalido.")
