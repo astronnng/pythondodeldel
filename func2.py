90870d52877dadd6031306a03e41dc83e3ddea0d
Astroooonngg <93302851+astronnng@users.noreply.github.com>
2025-10-01 12:08:51 -0300

    Usando def para listas

---
+++ func2.py
@@ -0,0 +1,6 @@
+def calcula_quadrado(lista):
+    for elem in lista:
+        quad = elem ** 2
+        print(f"O quadrado é: {elem} é {quad}")
+lista_de_inteiros = [1, 2, 3, 4, 5]
+calcula_quadrado(lista_de_inteiros) 
