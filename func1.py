92d0cb14b2b1afb0afa6cd2f3bb585c38a03a47f
Astroooonngg <93302851+astronnng@users.noreply.github.com>
2025-10-01 11:07:49 -0300

    Criado o primeiro teste com func usando DEF

---
+++ func1.py
@@ -0,0 +1,25 @@
+"""
+def encontra_maior(A,B):
+    if A > B:
+        return A
+    else:
+        return B
+    
+ 
+res = encontra_maior(10, 20)
+print(f"O valor maior é: {res}")
+
+"""
+
+
+def encontra_maior(A,B):
+    if A > B:
+        return A
+    else:
+        return B
+
+check1 = input(f"Digite um numero: ")
+check2 = input(f"Digite o segundo numero: ")
+
+res = encontra_maior(check1, check2)
+print(f"O valor maior é: {res}")
