dffcb20b91726511719832fd16d675a6a889c75c
Astroooonngg <93302851+astronnng@users.noreply.github.com>
2025-10-01 15:44:49 -0300

    Usado list com def

--- func2.py
+++ func2.py
@@ -1,6 +1,10 @@
-def calcula_quadrado(lista):
+def calcula_quadrado(lista):  #Definindo o que deve ser feito na DEF 
     for elem in lista:
         quad = elem ** 2
         print(f"O quadrado é: {elem} é {quad}")
-lista_de_inteiros = [1, 2, 3, 4, 5]
-calcula_quadrado(lista_de_inteiros) 
+
+
+lista_de_inteiros = [1, 2, 3, 4, 5] #Passando os valores que estao na lista 
+
+calcula_quadrado(lista_de_inteiros) #usando a def a def e passando onde ela tem que ver os valores 
+
